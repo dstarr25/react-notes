50020f92374a404022da3750d8e536b0b18f3172
dstarr25 <91504587+dstarr25@users.noreply.github.com>
2023-04-12 01:46:28 -0400

    previous favicon vite plugin didn't actually work, this one does though

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import eslint from 'vite-plugin-eslint';
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import autoprefixer from 'autoprefixer';
-import ViteFaviconsPlugin from 'vite-plugin-favicon';
+import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         eslint(),
-        ViteFaviconsPlugin('src/img/mm25.jpg'),
+        vitePluginFaviconsInject('src/img/mm25.jpg'),
     ],
     css: {
         postcss: {
