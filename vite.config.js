ffa2ed57ca3578e5d8cfe37d927c5e7ac8226714
dstarr25 <91504587+dstarr25@users.noreply.github.com>
2023-04-11 23:14:24 -0400

    favicon plugin added

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import eslint from 'vite-plugin-eslint';
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import autoprefixer from 'autoprefixer';
+import ViteFaviconsPlugin from 'vite-plugin-favicon';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         eslint(),
+        ViteFaviconsPlugin('src/img/mm25.jpg'),
     ],
     css: {
         postcss: {
