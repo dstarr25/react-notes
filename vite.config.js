7aa992750871c7ebc1b3547ac8317ec321ca52f7
dstarr25 <91504587+dstarr25@users.noreply.github.com>
2023-04-12 20:57:42 -0400

    got rid of favicons plugin, it actually didn't work

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,11 @@ import eslint from 'vite-plugin-eslint';
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import autoprefixer from 'autoprefixer';
-import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         eslint(),
-        vitePluginFaviconsInject('src/img/mm25.jpg'),
     ],
     css: {
         postcss: {
